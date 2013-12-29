445c707e3b9ec298d351bdfa582868207030f694
Susumu Ishizuka <susumu.ishizuka@kii.com>
2013-12-29 18:59:25 +0900

    cleaned up directory and added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='kiilib',
+    version='0.1dev',
+    packages=['kiilib',],
+    license='Apache 2.0',
+    long_description=open('README.rst').read(),
+)
