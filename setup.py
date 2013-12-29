5a67663ae0b4ac87b1b4a0a044cbcbf98a145f76
Susumu Ishizuka <susumu.ishizuka@kii.com>
2013-12-29 19:18:33 +0900

    added demo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='kiilib',
     version='0.1dev',
-    packages=['kiilib',],
+    packages=['kiilib','kiilib/demo'],
     license='Apache 2.0',
     long_description=open('README.rst').read(),
 )
